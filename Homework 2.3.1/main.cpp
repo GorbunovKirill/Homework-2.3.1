868b494e73e5319e4a6e5070573eebb713f59467
Kirill Gorbunov <4pduck@gmail.com>
2023-04-19 01:04:40 +0300

    new_new fixes

--- Homework 2.3.1/main.cpp
+++ Homework 2.3.1/main.cpp
@@ -2,94 +2,108 @@
 
 class Calculator {
 private:
-    double num1;
-    double num2;
+    double num1=7;
+    double num2=7;
 
 public:
     bool set_num1(double num1) {
-        if (num1 != 0) {
-            this->num1 = num1;
-            return false;
+        if (num1 == 0) {
+            
+            return true;
         }
-        else { return true; }
+        else { this->num1 = num1; return false; }
     }
     bool set_num2(double num2) {
-        if (num2 != 0) {
-            this->num2 = num2;
-            return false;
+        if (num2 == 0) {
+           
+            return true;
         }
-        else { return true; }
+        else { this->num2 = num2; return false; }
     }
-    double add(double num1, double num2) {
+    double add() {
         double sum = num1 + num2;
         return sum;
     }
-    double multiply(double num1, double num2) {
+    double multiply() {
         double sum = num1 * num2;
         return sum;
     }
-    double subtract_1_2(double num1, double num2) {
+    double subtract_1_2() {
         double sum = num1 - num2;
         return sum;
     }
-    double subtract_2_1(double num1, double num2) {
+    double subtract_2_1() {
         double sum = num2 - num1;
         return sum;
     }
-    double divide_1_2(double num1, double num2) {
+    double divide_1_2() {
         double sum = num1 / num2;
         return sum;
     }
-    double divide_2_1(double num1, double num2) {
+    double divide_2_1() {
         double sum = num2 / num1;
         return sum;
     }
-    Calculator(double num1, double num2) {
-        this->num1 = num1;
-        this->num2 = num2;
-    }
 
-    void calc_num(double num1, double num2) {
-        for (;;) {
-            do {
-                std::cout << "Введите num1:";
-                std::cin >> this->num1;
-                if (!std::cin) {
-                    std::cin.clear();
-                    std::cin.get();
-                }
-                this->set_num1(this->num1);
-                if (this->set_num1(this->num1)) { std::cout << "Неверный ввод!" << std::endl; }
-            } while (this->set_num1(this->num1));
-
-            do {
-                std::cout << "Введите num2:";
-                std::cin >> this->num2;
-                if (!std::cin) {
-                    std::cin.clear();
-                    std::cin.get();
-                }
-                this->set_num2(this->num2);
-                if (this->set_num2(this->num2)) { std::cout << "Неверный ввод!" << std::endl; }
-            } while (this->set_num2(this->num2));
-
-            std::cout << this->add(this->num1, this->num2) << std::endl;
-            std::cout << this->multiply(this->num1, this->num2) << std::endl;
-            std::cout << this->subtract_1_2(this->num1, this->num2) << std::endl;
-            std::cout << this->subtract_2_1(this->num1, this->num2) << std::endl;
-            std::cout << this->divide_1_2(this->num1, this->num2) << std::endl;
-            std::cout << this->divide_2_1(this->num1, this->num2) << std::endl;
-            std::cout << std::endl;
+   Calculator(double num1, double num2) {
+        if (num1 != 0) {
+            this->num1 = num1;
         }
+        else { num1 = 1; this->num1 = 1;}
+        
+    if (num2 !=0){ this->num2 = num2; }
+        else { num2 = 1; this->num2 = num2;}
     }
 };
 
 
+void calc_num(Calculator general) {
+        double number = 1;
+        double num1 = 2;
+        double num2 = 2;
+      
+        for (;;) {
+            do {
+                std::cout << "Введите num1:";
+        
+                std::cin >> num1;
+                if (!std::cin) {
+                    std::cin.clear();
+                    std::cin.get();
+                }
+                number = general.set_num1(num1);
+                if (number) { std::cout << "Неверный ввод!" << std::endl; }
+            } while (number);
+
+            do {
+                std::cout << "Введите num2:";
+                
+                std::cin >> num2;
+                if (!std::cin) {
+                    std::cin.clear();
+                    std::cin.get();
+                }
+             number = general.set_num2(num2);
+                if (number) { std::cout << "Неверный ввод!" << std::endl; }
+            } while (number);
+
+            std::cout << general.add() << std::endl;
+            std::cout << general.multiply() << std::endl;
+            std::cout << general.subtract_1_2() << std::endl;
+            std::cout << general.subtract_2_1() << std::endl;
+            std::cout << general.divide_1_2() << std::endl;
+            std::cout << general.divide_2_1() << std::endl;
+            std::cout << std::endl;
+        }
+    }
+
+
+
 int main() {
     setlocale(LC_ALL, "Russian");
-    double num1 = 0.0;
-    double num2 = 0.0;
+    double num1 = 1;
+    double num2 = 1;
     Calculator first(num1, num2);
-    first.calc_num(num1, num2);
+    calc_num(first);
     return 0;
 }
